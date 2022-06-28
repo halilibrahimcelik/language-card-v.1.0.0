99f45ac40c2664a40370993b20c64d60df3fcd9b
halilibrahimcelik <halil.celik139@gmail.com>
2022-06-28 12:45:17 +0300

    deployment of the project

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import "./scss/App.scss";
+
+function App() {
+  return <div></div>;
+}
+
+export default App;
---
+++ src/utils/data.js
@@ -0,0 +1,51 @@
+import css from "../img/css.png";
+import python from "../img/python.png";
+import java from "../img/java.png";
+import cpp from "../img/cpp.png";
+import go from "../img/go.png";
+import php from "../img/php.png";
+import javascript from "../img/javascript.png";
+import html from "../img/html.png";
+
+export const languages = [
+  {
+    name: "javascript",
+    img: javascript,
+    options: ["Website Development", "1995", "Brendan Eich"],
+  },
+  {
+    name: "html",
+    img: html,
+    options: ["Website Development", "1993", "Tim Berners-Lee"],
+  },
+  {
+    name: "css",
+    img: css,
+    options: ["Website Design", "1994", "Håkon Wium Lie"],
+  },
+  {
+    name: "python",
+    img: python,
+    options: ["AI", "1980", "Guido van Rossum"],
+  },
+  {
+    name: "java",
+    img: java,
+    options: ["BackEnd", "1990s", "Sun Microsystems"],
+  },
+  {
+    name: "c++",
+    img: cpp,
+    options: ["Game Dev.", "1980s", "Bjarne Stroustrup"],
+  },
+  {
+    name: "go",
+    img: go,
+    options: ["Network", "2009", "Robert Griesemer"],
+  },
+  {
+    name: "php",
+    img: php,
+    options: ["BackEnd", "1995", "	Rasmus Lerdorf"],
+  },
+];
