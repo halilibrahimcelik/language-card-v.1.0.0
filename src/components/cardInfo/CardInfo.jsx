0b91514d2544664075ccfca42a33179629c2fe22
halilibrahimcelik <halil.celik139@gmail.com>
2022-06-28 14:50:33 +0300

    Adding styling to card items.

--- src/components/cardInfo/CardInfo.jsx
+++ src/components/cardInfo/CardInfo.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import "./cardInfo.module.scss"
+import  styles from "./cardInfo.module.scss"
 export const CardInfo = (props) => {
     const {options,onVisibleHandler}=props;
   return (
-    <div>
+    <div className={styles.wrapper}>
          {options.map((option,index)=>{
   return(<ul key={index} onClick={onVisibleHandler}> <li  > {option}</li></ul>)
  })}
